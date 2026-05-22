3c1f0d07124754ad0d8c83725ec049c6f6c30e9f
Огурцы <kekov312@gmail.com>
2026-05-22 07:44:58 +0000

    strings done

---
+++ strings.js
@@ -0,0 +1,40 @@
+const sentence = "  javascript — это просто!  "
+const Date = "2025-03-15"
+const DateSplit = Date.split('-')
+const StudentFirstName = 'Иван'
+const StudentSecondName = 'Иванов'
+const StudentGroup = 'ИСР-21'
+const StudentAvgScore = 4.75
+const NodeJsDev = "Node.js разработчик"
+const NodeJsDevPointSplit = NodeJsDev.split(".")
+const NodeJsDevSpaceSplit = NodeJsDev.split(" ")
+
+
+const okak = "42"
+const okakLenght = okak.length
+
+const products = [
+    { name: "Ноутбук", price: 75000 },
+    { name: "Мышь", price: 1500 },
+    { name: "Монитор", price: 32000 },
+    { name: "Клавиатура", price: 3500 },
+    { name: "Наушники", price: 8000 }
+]
+
+
+.slice()
+
+console.log((`${(sentence.trim()).slice(0,1).toUpperCase()}${(sentence.trim()).slice(1)}`))
+
+console.log(" ")
+
+console.log(`Студент ${StudentFirstName} ${StudentSecondName}, группа ${StudentGroup}, средний балл: ${StudentAvgScore}`) // дебил дебилыч
+console.log(" ")
+
+console.log(`Строка начинается на Node? ${NodeJsDevPointSplit[0] == "Node" ? "Да" : "Нет"}, строка заканчивается на разработчик? ${NodeJsDevSpaceSplit[1] == "разработчик" ? "Да" : "Нет"}`)
+
+console.log(`${DateSplit[0]}.${DateSplit[1]}.${DateSplit[2]}`)
+console.log(" ")
+
+console.log(`Длинна строки ${okak} = ${okakLenght}`)
+console.log(`${"0".repeat(8 - okakLenght)}${okak}`)
