1c8c7b7b647ae2c159cbfa45d8c56397e3b98c1c
Огурцы <kekov312@gmail.com>
2026-05-23 09:46:43 +0000

    arrays done

--- arrays.js
+++ arrays.js
@@ -5,8 +5,10 @@ const products = [
   { name: "Клавиатура", price: 3500 },
   { name: "Наушники", price: 8000 }
 ]
+const productsCost = products.map(product => product.price)
 const StudentScore = [4, 5, 3, 5, 2, 4, 5, 3]
 const Fruits = ["банан", "яблоко", "вишня", "абрикос", "груша"]
+
 const FruitsSorted = Fruits.sort()
 const Numbers = [12, 45, 7, 33, 18]
 
@@ -14,5 +16,15 @@ console.log('Средний балл --',(StudentScore.reduce((acc, n) => acc + 
 console.log(' ')
 
 console.log((StudentScore.filter(n => n>3)).map(n => `Оценка ${n}`))
+console.log(' ')
+
 
 console.log(FruitsSorted.join(', ',5))
+console.log(' ')
+
+console.log(products.filter(product => product.price < 10000))
+console.log(products.map(product => product.name))
+console.log(productsCost.reduce((acc, n) => acc + n, 0))
+console.log(' ')
+
+console.log(`Есть ли число больше 40? ${Numbers.some(n => n>40)} Все ли числа больше 40? ${Numbers.every(n => n>40)}`)
---
+++ objects.js
@@ -0,0 +1,10 @@
+const students = [
+  { name: "Анна", grade: 4.8, passed: true },
+  { name: "Борис", grade: 2.9, passed: false },
+  { name: "Вера", grade: 3.5, passed: true },
+  { name: "Григорий", grade: 4.1, passed: true },
+  { name: "Дарья", grade: 2.5, passed: false },
+]
+
+const u1 = { name: "Иван", age: 20, city: "Казань" }
+const u2 = { name: "Иван Петров", email: "ivan@mail.ru", city: "Москва" }
